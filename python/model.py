fad3054cb5ed5a52ca82f7ea28c18c2240c1dca2
Vimagoru <gonzalezvmruiz@gmail.com>
2021-01-14 15:23:55 -0500

    Cambio en los datos, variables dummis

--- python/model.py
+++ python/model.py
@@ -5,14 +5,19 @@ import pickle
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import LSTM
+from tensorflow.keras.layers import SimpleRNN
 from tensorflow.keras.layers import Dropout
 
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import TweedieRegressor
+from sklearn.linear_model import GammaRegressor
 
 # TODO: import your modules here.
 # Don't forget to add them to requirements.txt before submitting.
@@ -26,13 +31,23 @@ df=pd.read_csv("training.csv")
 
 
 
-df[["Min","Med1","Med2","Max"]]=pd.get_dummies(df.pol_coverage)
-df[['Biannual', 'Monthly', 'Quarterly', 'Yearly']]=pd.get_dummies(df.pol_pay_freq)
-df[['AllTrips', 'Professional', 'Retired', 'WorkPrivate']]=pd.get_dummies(df.pol_usage)
-df[['Sex2_0', 'Sex2_F', 'Sex2_M']]=pd.get_dummies(df.drv_sex2)
-df[['Diesel', 'Gasoline', 'Hybrid']]=pd.get_dummies(df.vh_fuel)
-df[['Commercial', 'Tourism']]=pd.get_dummies(df.vh_type)
+# reference level: Max 
+df[["Min","Med1","Med2"]]=pd.get_dummies(df.pol_coverage,drop_first=True)
 
+# reference level: Biannual 
+df[['Monthly', 'Quarterly', 'Yearly']]=pd.get_dummies(df.pol_pay_freq,drop_first=True)
+
+# reference level: AllTrips
+df[['Professional', 'Retired', 'WorkPrivate']]=pd.get_dummies(df.pol_usage,drop_first=True)
+
+# reference level: Sex0 
+df[['Sex2_F', 'Sex2_M']]=pd.get_dummies(df.drv_sex2,drop_first=True)
+
+# reference level: Diesel
+df[['Gasoline', 'Hybrid']]=pd.get_dummies(df.vh_fuel,drop_first=True)
+
+
+df["vh_type"]=df["vh_type"].replace("'Commercial'",1).replace('Tourism',0)
 df["pol_payd"]=df["pol_payd"].replace("No",1).replace("Yes",0)
 df["drv_drv2"]=df["drv_drv2"].replace("No",1).replace("Yes",0)
 df["drv_sex1"]=df["drv_sex1"].replace("F",1).replace("M",0)
@@ -40,7 +55,9 @@ df["drv_age2"]=df["drv_age2"].fillna(0)
 df["drv_age_lic2"]=df["drv_age_lic2"].fillna(0)
 
 df=df.drop(["pol_coverage","pol_pay_freq","pol_usage",\
-                "drv_sex2","vh_fuel","vh_type","vh_make_model"],axis=1)
+                "drv_sex2","vh_fuel","vh_make_model"],axis=1)
+
+df['id_positive'] = pd.DataFrame([ i>0 for i in df['claim_amount']])
 
 df=df.dropna()
 id_train, id_test= train_test_split(df["id_policy"].unique(),test_size=0.20, random_state=5)
@@ -61,7 +78,17 @@ df4=df[df["year"]==4].drop("year",axis=1).sort_values(by=["id_policy"]).reset_in
 scaler4 = MinMaxScaler()
 df4["claim_amount"] = pd.DataFrame(scaler4.fit_transform(np.array(df4["claim_amount"]).reshape(-1, 1)))
 
+
+
+
+
 claim_amount_train = np.array(df4[df4["id_policy"].isin(id_train)]["claim_amount"])
+
+claim_amount_train3D = np.array(list(zip(df1[df4["id_policy"].isin(id_train)]["claim_amount"].values.tolist(),
+                                         df2[df4["id_policy"].isin(id_train)]["claim_amount"].values.tolist(),
+                                         df3[df4["id_policy"].isin(id_train)]["claim_amount"].values.tolist())))
+
+
 claim_amount_test = np.array(df4[df4["id_policy"].isin(id_test)]["claim_amount"])
 
 
@@ -76,30 +103,47 @@ label_dis=["Min","Med1","Med2","Max",'Biannual', 'Monthly', 'Quarterly',\
         'Sex2_0', 'Sex2_F', 'Sex2_M','Diesel', 'Gasoline', 'Hybrid','Commercial', 'Tourism']
 
 
-#X_Training_Data=np.array(list(zip(df1.drop("claim_amount",axis=1).values.tolist(),
-#                                  df2.drop("claim_amount",axis=1).values.tolist(),
-#                                  df3.drop("claim_amount",axis=1).values.tolist())))
 
 
-X_Training_Data=np.array(list(zip(df1[df1["id_policy"].isin(id_train)].drop("id_policy",axis=1).values.tolist(),
-                                  df2[df2["id_policy"].isin(id_train)].drop("id_policy",axis=1).values.tolist(),
-                                  df3[df3["id_policy"].isin(id_train)].drop("id_policy",axis=1).values.tolist())))
+# X_Training_Data=np.array(list(zip(df1[df1["id_policy"].isin(id_train)].drop("id_policy",axis=1).values.tolist(),
+#                                   df2[df2["id_policy"].isin(id_train)].drop("id_policy",axis=1).values.tolist(),
+#                                   df3[df3["id_policy"].isin(id_train)].drop("id_policy",axis=1).values.tolist())))
+
+X_Training_Data=np.array(list(zip(df1[df1["id_policy"].isin(id_train)].drop(["id_policy","claim_amount"],axis=1).values.tolist(),
+                                  df2[df2["id_policy"].isin(id_train)].drop(["id_policy","claim_amount"],axis=1).values.tolist(),
+                                  df3[df3["id_policy"].isin(id_train)].drop(["id_policy","claim_amount"],axis=1).values.tolist())))
 
 
-X_Test_Data=np.array(list(zip(df1[df1["id_policy"].isin(id_test)].drop("id_policy",axis=1).values.tolist(),
-                              df2[df2["id_policy"].isin(id_test)].drop("id_policy",axis=1).values.tolist(),
-                              df3[df3["id_policy"].isin(id_test)].drop("id_policy",axis=1).values.tolist())))
+# X_Test_Data=np.array(list(zip(df2[df1["id_policy"].isin(id_test)].drop("id_policy",axis=1).values.tolist(),
+#                               df3[df2["id_policy"].isin(id_test)].drop("id_policy",axis=1).values.tolist(),
+#                               df4[df3["id_policy"].isin(id_test)].drop("id_policy",axis=1).values.tolist())))
 
-      
+X_Test_Data=np.array(list(zip(df2[df1["id_policy"].isin(id_test)].drop(["id_policy","claim_amount"],axis=1).values.tolist(),
+                              df3[df2["id_policy"].isin(id_test)].drop(["id_policy","claim_amount"],axis=1).values.tolist(),
+                              df4[df3["id_policy"].isin(id_test)].drop(["id_policy","claim_amount"],axis=1).values.tolist())))
+    
 
 
-# model   
+# Model 1
 #%%    
 rnn = Sequential()
 
+#Adding our first SimpleRNN layer
+
+rnn.add(SimpleRNN(units = 1,return_sequences=True,activation="sigmoid",input_shape = (3, X_Training_Data.shape[2])))
+
+
 #Adding our first LSTM layer
 
-rnn.add(LSTM(units = 1, input_shape = (3, X_Training_Data.shape[2])))
+#rnn.add(LSTM(units = 1,return_sequences = True,input_shape = (3, X_Training_Data.shape[2])))
+
+#Perform some dropout regularization
+
+#rnn.add(Dropout(0.2))
+
+#Adding our first LSTM layer
+
+#rnn.add(LSTM(units = 1, input_shape = (3, X_Training_Data.shape[2])))
 
 #Perform some dropout regularization
 
@@ -107,20 +151,24 @@ rnn.add(Dropout(0.2))
 
 #Adding our output layer
 
-rnn.add(Dense(units = 1))
+#rnn.add(Dense(units = 1))
 
     
 #Compiling the recurrent neural network
 
 rnn.compile(optimizer = 'adam', loss = 'mean_squared_error')
 
+# Summary
+
+rnn.summary()
+
 #Training the recurrent neural network
 
-rnn.fit(X_Training_Data, claim_amount_train, epochs = 100)
+
+rnn.fit(X_Training_Data, claim_amount_train3D, epochs = 1500,batch_size = 150)
 
 predictions = rnn.predict(X_Test_Data)
 
-plt.plot(predictions)
 
 unscaled_predictions = scaler4.inverse_transform(predictions)
 
@@ -130,6 +178,49 @@ unscaled_resutados = np.array(scaler4.inverse_transform(claim_amount_test.reshap
 # in your model (e.g., a preprocessing function).
 
 
+
+# Model 2
+#%% 
+
+
+lr=LogisticRegression(random_state=0).fit(df2[df2["id_policy"].isin(id_train)].drop(["id_policy","claim_amount","id_positive"],axis=1),
+                                          df2[df2["id_policy"].isin(id_train)]["id_positive"])
+
+lr.score(df2[df2["id_policy"].isin(id_test)].drop(["id_policy","claim_amount","id_positive"],axis=1),
+         df2[df2["id_policy"].isin(id_test)]["id_positive"])
+
+
+reg = TweedieRegressor(power=2, alpha=0.5, link='log')
+reg.fit(df1[df1["id_policy"].isin(id_train) & df1["id_positive"]==True].drop(["id_policy","claim_amount","id_positive"],axis=1),
+        df1[df1["id_policy"].isin(id_train) & df1["id_positive"]==True]["claim_amount"])
+
+Pre = reg.predict(df1[df1["id_policy"].isin(id_test) & df1["id_positive"]==True].drop(["id_policy","claim_amount","id_positive"],axis=1))
+
+
+reg.score(df1[df1["id_policy"].isin(id_test) & df1["id_positive"]==True].drop(["id_policy","claim_amount","id_positive"],axis=1),
+          df1[df1["id_policy"].isin(id_test) & df1["id_positive"]==True]["claim_amount"])
+
+
+sns.heatmap(np.corrcoef(df1[df1["id_policy"].isin(id_train) & df1["id_positive"]==True].drop(["id_policy"],axis=1).values.tolist(),rowvar=False))
+
+
+# Create correlation matrix 
+corr_mat = df1[df1["id_policy"].isin(id_train) & df1["id_positive"]==True].drop(["id_policy"],axis=1).corr(method='pearson') 
+  
+# Retain upper triangular values of correlation matrix and 
+# make Lower triangular values Null 
+upper_corr_mat = corr_mat.where( 
+    np.triu(np.ones(corr_mat.shape), k=1).astype(np.bool)) 
+  
+# Convert to 1-D series and drop Null values 
+unique_corr_pairs = upper_corr_mat.unstack().dropna() 
+  
+# Sort correlation pairs 
+sorted_mat = unique_corr_pairs.sort_values() 
+print(sorted_mat) 
+
+
+#%%
 
 def fit_model(X_raw, y_raw):
     """Model training function: given training data (X_raw, y_raw), train this pricing model.
